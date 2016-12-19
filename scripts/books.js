52d3fa68b584850bddbb15cb1c3fa189c9574d5a
mickata <kozarovdimitar@gmail.com>
2016-12-19 15:29:38 +0200

    40% of the blog is done. I have done the login and logout forms, they are working correctly with "Kinvey"

--- scripts/books.js
+++ scripts/books.js
@@ -1,7 +1,29 @@
+const kinveyAppID = 'kid_Bk_0BMN4x';
+const kinveyAppSecret = 'aae74caf18054b2490dbc13e8e0c7be5';
+const kinveyServiceBaseUrl = 'https://baas.kinvey.com/';
+
+
 function showView(viewId) {
     $("main > section").hide();
 
     $("#" + viewId).show();
+}
+
+function showHideNavigationLinks() {
+    let loggedIn = sessionStorage.authToken != null;
+    if(loggedIn){
+        $("#linkLogin").hide();
+        $("#linkRegister").hide();
+        $("#linkListBooks").show();
+        $("#linkCreateBook").show();
+        $("#linkLogout").show();
+    } else {
+        $("#linkLogin").show();
+        $("#linkRegister").show();
+        $("#linkListBooks").hide();
+        $("#linkCreateBook").hide();
+        $("#linkLogout").hide();
+    }
 }
 
 function showHomeView() {
@@ -12,8 +34,45 @@ function showLoginView() {
     showView('viewLogin');
 }
 
+function login() {
+    let authBase64 = btoa(kinveyAppID + ":" + kinveyAppSecret);
+    let loginUrl = kinveyServiceBaseUrl + "user/" + kinveyAppID + "/login";
+    let loginData = {
+        username: $("#loginUser").val(),
+        password: $("#loginPass").val(),
+    };
+    $.ajax({
+        method: "POST",
+        url: loginUrl,
+        data: loginData,
+        headers: { "Authorization": "Basic " + authBase64},
+        success: loginSuccess,
+        error: showAjaxError
+    });
+    function loginSuccess(data, status) {
+        sessionStorage.authToken = data._kmd.authtoken;
+        showListBooksView();
+        showHideNavigationLinks();
+        showInfo("Login successfull");
+    }
+}
+
+function showInfo(messageText) {
+    $("#infoBox").text(messageText).show().delay(3000).fadeOut();
+}
+
+function showAjaxError(data, status) {
+    let  errorMsg = "Error: " + JSON.stringify(data);
+    $('#errorBox').text(errorMsg).show();
+}
+
+
 function showRegisterView() {
     showView('viewRegister');
+}
+
+function register() {
+    
 }
 
 function showListBooksView() {
@@ -24,12 +83,15 @@ function showCreateBookView() {
     showView('viewCreateBook');
 }
 
-function logout() {
-    alert('logout');
-    showHomeView();
+function createBook() {
+
 }
 
-
+function logout() {
+    sessionStorage.clear();
+    showHomeView();
+    showHideNavigationLinks();
+}
 
 $(function () {
     $("#linkHome").click(showHomeView);
@@ -38,4 +100,11 @@ $(function () {
     $("#linkListBooks").click(showListBooksView);
     $("#linkCreateBook").click(showCreateBookView);
     $("#linkLogout").click(logout);
+
+    $("#buttonLogin").click(login);
+    $("#buttonRegister").click(register);
+    $("#buttonCreateBook").click(createBook);
+
+    showHomeView();
+    showHideNavigationLinks();
 });
